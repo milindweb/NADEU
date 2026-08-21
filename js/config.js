6c977eee277171d7b1a80523a2ef9365cd607d8e
milindweb <milindweb@gmail.com>
2026-08-21 18:08:55 +0530

    Update API_URL with deployed Web App URL

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 
 const CONFIG = {
   // UPDATE THIS after deploying Apps Script as Web App
-  API_URL: 'https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxKBirM-6nP-PHHvZswfS4FwoRuvOk5n6ocYUD3wZr_6uGqz7eTcOjVOS0BKfhiFvYocg/exec',
   MODULE: 'receipt',
   STORAGE_KEYS: {
     TOKEN: 'nadeu_token',
